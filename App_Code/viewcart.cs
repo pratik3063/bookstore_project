1793d0fb7acc549afa6d94c2ac82846da48b21d7
pratik3063 <pratik30393@gmail.com>
2017-07-19 16:12:31 +0530

    Add files via upload

---
+++ App_Code/viewcart.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Web.UI.WebControls;
+
+
+
+/// <summary>
+/// Summary description for viewcart
+/// </summary>
+public class viewcart 
+{
+        SqlConnection con = new SqlConnection(WebConfigurationManager.ConnectionStrings["bookcartConnectionString"].ConnectionString);
+    int cart1;
+	public viewcart()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+  public int cart()
+    {
+      
+        con.Open();
+         SqlCommand sum = new SqlCommand("select count(*) from bookcart4", con);
+        cart1 = (int)sum.ExecuteScalar();
+        con.Close();
+        return cart1;
+      
+       
+      
+    
+    }
+}
